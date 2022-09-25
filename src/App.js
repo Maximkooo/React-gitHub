efc905e659e891a52f884dd86f0d2a9f93d4fd60
Maximkooo <43643606+Maximkooo@users.noreply.github.com>
2022-09-25 14:28:35 +0200

    task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: 'Stepan',
+      age: 25,
+      isShow: false
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+   handleChange() {
+    this.setState({name: 'Mykola', age: 30, isShow: !this.state.isShow})
+  }
+
+  render(){
+    return (
+      <div>
+        { this.state.isShow ?
+          <p> Name: {this.state.name}, age: {this.state.age} </p> :
+          ''
+        }
+	      <button onClick={this.handleChange}>{ !this.state.isShow ? 'показать' : 'скрыть'}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
