1b59081475049d815df161f10fd31ba7989f18fc
Maximkooo <43643606+Maximkooo@users.noreply.github.com>
2022-10-13 13:09:32 +0200

    main

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,6 @@ import React from 'react';
 import './App.css';
 
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
   render(){
     return (
       <div className="center">
