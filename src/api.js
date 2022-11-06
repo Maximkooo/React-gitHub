3c52ca3372c74c3deca0c029ee2fa00f3ca2d685
Maximkooo <43643606+Maximkooo@users.noreply.github.com>
2022-11-06 14:28:08 +0100

    main

--- src/api.js
+++ src/api.js
@@ -5,12 +5,47 @@ export const fetchPopularRepos = (language) => {
   .then(response => response.data.items)
 }
 
-export const getUser = (user) => {
-  return axios.get(`https://api.github.com/users/${user}`)
-  .then(response => response.data)
+const handleError = (error) => console.log(error);
+
+const getProfile = (username) => {
+  return axios.get(`https://api.github.com/users/${username}`)
+  .then(user => user.data)
 }
 
-export const getUserRepos = (user) => {
-  return axios.get(`https://api.github.com/users/${user}/repos`)
-  .then(response => response.data)
+const getRepos = (username) => {
+  return axios.get(`https://api.github.com/users/${username}/repos`)
+  .then(repos => repos.data)
 }
+const getStarCount = (repos) => {
+  return repos.reduce((acc, repo) => {
+    return acc + repo.stargazers_count;
+  }, 0)
+}
+const calculateScore = (profile, repos) => {
+  const followers = profile.followers
+  const totalStars = getStarCount(repos)
+  return followers + totalStars
+}
+
+const getUserData = (username) => {
+  return Promise.all([
+    getProfile(username),
+    getRepos(username)
+  ])
+    .then(([profile, repos]) => {
+      return {
+        profile: profile,
+        score: calculateScore(profile, repos)
+      }
+    })
+}
+
+ const sortPlayers = (player) =>{
+  return player.sort((a, b) => b.score - a.score)
+ }
+
+ export const startBattle = (players) => {
+  return Promise.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError)
+ }
--- src/components/Battle/Results.js
+++ src/components/Battle/Results.js
@@ -1,116 +1,51 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { getUser, getUserRepos } from "../../api";
+import {startBattle} from "../../api.js"
+
 import PlayerPreview from "./PlayerPreview";
 
 const Results = () => {
-  const [playerOneFollowers, setPlayerOneFollowers] = useState(0)
-  const [playerOneStarsRepos, setPlayerOneStarsRepos] = useState(0)
-  const [playerOneResult, setPlayerOneResult] = useState(0)
-  const [playerOneImage, setPlayerOneImage] = useState('')
-  const [playerOneName, setPlayerOneName] = useState('')
-
-  const [playerTwoFollowers, setPlayerTwoFollowers] = useState(0)
-  const [playerTwoStarsRepos, setPlayerTwoStarsRepos] = useState(0)
-  const [playerTwoResult, setPlayerTwoResult] = useState(0)
-  const [playerTwoImage, setPlayerTwoImage] = useState('')
-  const [playerTwoName, setPlayerTwoName] = useState('')
-
-  const [loader, setLoader] = useState(true);
+  const [winner, setWinner] = useState(null)
+  const [loser, setLoser] = useState(null)
+  const [loader, setLoader] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
-    const playerOne = getSearchParams('playerOneName')
-    const playerTwo = getSearchParams('playerTwoName')
-    const players = [playerOne, playerTwo]
-    setPlayerOneImage(`https://github.com/${playerOne}.png?size=200`)
-    setPlayerTwoImage(`https://github.com/${playerTwo}.png?size=200`)
-    for (let index = 0; index < players.length; index++) {
-      getUserData(players[index], index)
-      getUserStarsRepos(players[index], index)
-    }
-  },[])
-
-  useEffect(() => {
-    setPlayerOneResult(playerOneStarsRepos + playerOneFollowers)
-    setPlayerTwoResult(playerTwoStarsRepos + playerTwoFollowers)
-    setLoader(false)
-  },[playerOneStarsRepos, playerTwoStarsRepos])
-
-  const getSearchParams = (player) => {
     const searchParams = new URLSearchParams(location.search)
-    return searchParams.get(player)
-  }
+    startBattle([searchParams.get('playerOneName'), searchParams.get('playerTwoName')])
+      .then(([winner, loser]) => {
+        setWinner(winner);
+        setLoser(loser);
+        setLoader(true)
+      })
+  },[location.search])
 
-  const getUserData = (player, index) => {
-    getUser(player)
-      .then(data => {
-        if (index === 0) {
-          setPlayerOneFollowers(data.followers)
-          setPlayerOneName(data.login)
-        }
-        else {
-          setPlayerTwoFollowers(data.followers)
-          setPlayerTwoName(data.login)
-        }
-      })
-      .catch(error => {
-        console.log(error);
-    })
-  }
-  const getUserStarsRepos = (player, index) => {
-    getUserRepos(player)
-      .then(data => {
-        data.forEach((item)=> {
-          index === 0 ?
-          setPlayerOneStarsRepos(prevTest => prevTest + item.stargazers_count) :
-          setPlayerTwoStarsRepos(prevTest => prevTest + item.stargazers_count)
-        })
-      })
-      .catch(error => {
-        console.log(error);
-    })
-  }
 
   return (
     <div className="row">
-      { loader === true ? <div className="loader"></div> :
-        playerOneResult >= playerTwoResult ?
-          <>
-            <PlayerPreview
-              avatar={playerOneImage}
-              username={playerOneName}
-            >
-              <h4 className="winner">{playerOneResult}</h4>
-              <h1 className="winner">Winner</h1>
-            </PlayerPreview>
+      { loader ?
+        <>
+          <PlayerPreview
+            avatar={winner.profile.avatar_url}
+            username={winner.profile.login}
+          >
+            <h3 className="score_title">
+              Score: <span className="score">{winner.score}</span>
+            </h3>
+          </PlayerPreview>
 
-            <PlayerPreview
-              avatar={playerTwoImage}
-              username={playerTwoName}
-            >
-              <h4 className="loser">{playerTwoResult}</h4>
-              <h1 className="loser">Loser</h1>
-            </PlayerPreview>
-          </> :
-          <>
-            <PlayerPreview
-              avatar={playerTwoImage}
-              username={playerTwoName}
-            >
-              <h4 className="winner">{playerTwoResult}</h4>
-              <h1 className="winner">Winner</h1>
-            </PlayerPreview>
-
-            <PlayerPreview
-              avatar={playerOneImage}
-              username={playerOneName}
-            >
-              <h4 className="loser">{playerOneResult}</h4>
-              <h1 className="loser">Loser</h1>
-            </PlayerPreview>
-          </>
+          <PlayerPreview
+            avatar={loser.profile.avatar_url}
+            username={loser.profile.login}
+          >
+            <h3 className="score_title">
+              Score: <span className="score">{loser.score}</span>
+            </h3>
+          </PlayerPreview>
+        </>
+        : null
       }
+      
     </div>
   )
 }
